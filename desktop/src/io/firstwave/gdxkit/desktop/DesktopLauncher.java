7fa3cf6468efce2eec49e2aaa365b5189c3a1819
Oliver Bartley <oliver@synapticats.com>
2015-01-09 22:10:30 -0800

    initial commit

---
+++ desktop/src/io/firstwave/gdxkit/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.firstwave.gdxkit.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.firstwave.gdxkit.GdxKitGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxKitGame(), config);
+	}
+}
